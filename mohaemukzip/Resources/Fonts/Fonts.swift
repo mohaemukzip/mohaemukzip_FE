253e9ae2ac0b44a4e719289e9fd34cb8e4d8c350
21g <leehan0311@gmail.com>
2026-01-09 12:46:56 +0900

    [Feat] Font 컴포 생성, (.gitignore 진짜 적용 및 정크파일 추적 중단.)

--- mohaemukzip/Resources/Fonts/Fonts.swift
+++ mohaemukzip/Resources/Fonts/Fonts.swift
@@ -6,3 +6,74 @@
 //
 
 import Foundation
+import SwiftUI
+
+extension Font {
+    enum Pretend {
+        case bold
+        case semiBold
+        case medium
+        case regular
+        
+        var value: String {
+            switch self {
+            case .bold:
+                return "Pretendard-Bold"
+            case .semiBold:
+                return "Pretendard-SemiBold"
+            case .medium:
+                return "Pretendard-Medium"
+            case .regular:
+                return "Pretendard-Regular"
+            }
+        }
+    }
+    
+    static func pretend(type: Pretend, size: CGFloat) -> Font {
+        return .custom(type.value, size: size)
+    }
+    
+    static var PretendardBold26: Font {
+        return pretend(type: .bold, size: 26)
+    }
+    
+    static var PretendardBod24: Font {
+        return pretend(type: .bold, size: 24)
+    }
+    
+    static var PretendardSemibold20: Font {
+        return pretend(type: .semiBold, size: 20)
+    }
+    
+    static var PretendardSemibold18: Font {
+        return pretend(type: .semiBold, size: 18)
+    }
+    
+    static var PretendardSemibold16: Font {
+        return pretend(type: .semiBold, size: 16)
+    }
+    
+    static var PretendardMedium16: Font {
+        return pretend(type: .medium, size: 16)
+    }
+    
+    static var PretendardMedium13: Font {
+        return pretend(type: .medium, size: 13)
+    }
+    
+    static var PretendardMedium12: Font {
+        return pretend(type: .medium, size: 12)
+    }
+    
+    static var PretendardRegular16: Font {
+        return pretend(type: .regular, size: 16)
+    }
+    
+    static var PretendardRegular14: Font {
+        return pretend(type: .regular, size: 14)
+    }
+    
+    static var PretendardRegular13: Font {
+        return pretend(type: .regular, size: 13)
+    }
+}
