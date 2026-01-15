108f52267015dec0e53effbb282af17ce1a042ba
gogo <daehcjf@gmail.com>
2026-01-15 16:00:05 +0900

    [Feat] MainTabView
    탭뷰 수정

--- mohaemukzip/Sources/Feature/Main/MainTabView.swift
+++ mohaemukzip/Sources/Feature/Main/MainTabView.swift
@@ -2,25 +2,74 @@
 //  MainTabView.swift
 //  mohaemukzip
 //
-//  Created by 이한결 on 1/13/26.
+//  Created by 고석현 on 1/15/26.
 //
 
 import SwiftUI
 
 struct MainTabView: View {
+    enum TabType: Hashable {
+        case home
+        case search
+        case ingredient
+        case profile
+    }
+    
+    //기본 설정된 상태 -> home
+    @State private var selection: TabType = .home
+
+    init() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+
+        // 선택되지 않은 상태 (grey500)
+        appearance.stackedLayoutAppearance.normal.iconColor = UIColor(named: "grey500")
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = [
+            .foregroundColor: UIColor(named: "grey500") as Any
+        ]
+
+        // 선택된 상태 (grey900)
+        appearance.stackedLayoutAppearance.selected.iconColor = UIColor(named: "grey900")
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = [
+            .foregroundColor: UIColor(named: "grey900") as Any
+        ]
+
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
     var body: some View {
-        TabView {
-            Tab("홈", image: "icon-home") {
+        TabView(selection: $selection) {
+            Tab(value: .home) {
                 HomeView()
+            } label: {
+                Label(
+                    "홈",
+                    image: selection == .home ? "selectedHome" : "icon-home"
+                )
             }
-            Tab("검색", image: "icon-search") {
+            Tab(value: .search) {
                 SearchView()
+            } label: {
+                Label(
+                    "검색",
+                    image: selection == .search ? "selectedSearch" : "icon-search"
+                )
             }
-            Tab("재료", image: "icon-ingredient") {
+            Tab(value: .ingredient) {
                 IngredientView()
+            } label: {
+                Label(
+                    "재료",
+                    image: selection == .ingredient ? "selectedIngredient" : "icon-ingredient"
+                )
             }
-            Tab("마이", image: "icon-profile") {
+            Tab(value: .profile) {
                 ProfileView()
+            } label: {
+                Label(
+                    "마이",
+                    image: selection == .profile ? "selectedProfile" : "icon-profile"
+                )
             }
         }
     }
