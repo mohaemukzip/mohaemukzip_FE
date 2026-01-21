062a64f7e23f0c1ae7d1142d444c4fcf3e05b2b2
21g <leehan0311@gmail.com>
2026-01-21 14:51:33 +0900

    [Feat] 재료 리스트 컴포넌트

---
+++ mohaemukzip/Sources/Common/Components/IngredientListComponent/IngredientListComponent.swift
@@ -0,0 +1,48 @@
+//
+//  IngredientListComponent.swift
+//  mohaemukzip
+//
+//  Created by 이한결 on 1/21/26.
+//
+
+import SwiftUI
+
+struct IngredientListComponent: View {
+    let name: String
+    let amount: String
+    @State var isSaved: Bool = false
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(name)
+                        .foregroundStyle(.grey900)
+                        .font(.PretendardMedium16)
+                    Button( action: { isSaved.toggle() } ) {
+                        Image("icon-star")
+                            .foregroundStyle(isSaved ? .main300 : .grey300)
+                    }
+                }
+                Text(amount)
+                    .foregroundStyle(.grey500)
+                    .font(.PretendardRegular13)
+            }
+            
+            Spacer()
+            
+            Button( action: { } ) {
+                Image("icon-plus")
+                    .frame(width: 32, height: 32)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: 96)
+        .padding()
+    }
+}
+
+
+#Preview {
+    IngredientListComponent(name: "대파", amount: "1기본량(100g)")
+}
+
