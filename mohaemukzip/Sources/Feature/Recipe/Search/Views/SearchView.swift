ab9415f4664a86428fef42617bf088796285d528
21g <leehan0311@gmail.com>
2026-02-06 15:31:56 +0900

    [Fix], [Design] 검색 뷰, 냉장고 뷰 초기화 로직
    검색 뷰, 냉장고 뷰에서 onDisappear를 활용해 초기화 로직 삽입

--- mohaemukzip/Sources/Feature/Recipe/Search/Views/SearchView.swift
+++ mohaemukzip/Sources/Feature/Recipe/Search/Views/SearchView.swift
@@ -71,6 +71,10 @@ struct SearchView: View {
                     await viewModel.resetAndSearch()
                 }
             }
+            .onDisappear {
+                // MARK: 뷰가 사라질 때 searchText 초기화
+                viewModel.searchText = ""
+            }
     }
 }
 
