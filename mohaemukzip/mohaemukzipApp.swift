59945fa08a4d50f09556065f4b40901b809c5a6b
21g <leehan0311@gmail.com>
2026-07-02 15:54:30 +0900

    [Docs] 카카오 SDK 의존성 추가, 관련 설정 진행

--- mohaemukzip/mohaemukzipApp.swift
+++ mohaemukzip/mohaemukzipApp.swift
@@ -1,11 +1,32 @@
 import SwiftUI
+import KakaoSDKCommon
+import KakaoSDKAuth
 
 @main
 struct mohaemukzipApp: App {
+    
+    init() {
+        guard let appKey = Bundle.main.object(
+            forInfoDictionaryKey: "KAKAO_NATIVE_APP_KEY"
+        ) as? String,
+        !appKey.isEmpty else {
+            assertionFailure("KAKAO_NATIVE_APP_KEY가 설정되지 않았습니다.")
+            return
+        }
+
+        KakaoSDK.initSDK(appKey: appKey)
+    }
+    
+    
     var body: some Scene {
         WindowGroup {
             RootView()
                 .preferredColorScheme(.light)
+                .onOpenURL { url in
+                    if AuthApi.isKakaoTalkLoginUrl(url) {
+                        _ = AuthController.handleOpenUrl(url: url)
+                    }
+                }
         }
     }
 }
