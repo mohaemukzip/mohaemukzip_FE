8d29c39d2ced93047668ec9e5f2bb112fb0376e7
gogo <daehcjf@gmail.com>
2026-07-21 15:59:38 +0900

    [충돌]

--- mohaemukzip/Service/Networking/Auth/AuthAPI.swift
+++ mohaemukzip/Service/Networking/Auth/AuthAPI.swift
@@ -1,3 +1,4 @@
+
 import Foundation
 import Moya
 import Alamofire
@@ -6,13 +7,6 @@ enum AuthAPI {
     case signup(SignUpRequestDTO)
     case login(LoginRequestDTO)
     case checkLoginId(CheckLoginIdRequestDTO)
-
-    // MARK: - Email
-    case sendResetPasswordEmail(AuthRequestDTO.SendEmailVerificationRequest)
-    case verifyEmail(AuthRequestDTO.VerifyEmailRequest)
-    case resetPassword(AuthRequestDTO.ResetPasswordRequest)
-    case requestEmailVerification(SendEmailVerificationRequestDTO)
-
     case reissue
     case logout
     case withdrawal
@@ -23,90 +17,54 @@ extension AuthAPI: TargetType {
         print("Config.baseURL =", Config.baseURL)
         return URL(string: Config.baseURL)!
     }
-
+    
     var path: String {
         switch self {
         case .signup:
             return "/auth/signup"
-
         case .login:
             return "/auth/login"
-
         case .checkLoginId:
             return "/auth/check-loginid"
-
-        case .sendResetPasswordEmail:
-            return "/auth/email/send/reset-password"
-        case .requestEmailVerification:
-            return "/auth/email/send"
-
-        case .verifyEmail:
-            return "/auth/email/verify"
-
-        case .resetPassword:
-            return "/auth/reset-password"
-
         case .reissue:
             return "/auth/reissue"
-
         case .logout:
             return "/auth/logout"
-
         case .withdrawal:
             return "/auth/withdrawal"
         }
     }
-
+    
     var method: Moya.Method {
         switch self {
-        case .signup,
-             .login,
-             .checkLoginId,
-             .sendResetPasswordEmail,
-             .requestEmailVerification,
-             .verifyEmail,
-             .reissue,
-             .logout:
+        case .signup:
             return .post
-
-        case .resetPassword:
-            return .patch
-
+        case .login:
+            return .post
+        case .checkLoginId:
+            return .post
+        case .reissue:
+            return .post
+        case .logout:
+            return .post
         case .withdrawal:
             return .delete
         }
     }
-
+    
     var task: Task {
         switch self {
         case .signup(let request):
             return .requestJSONEncodable(request)
-
         case .login(let request):
             return .requestJSONEncodable(request)
-
         case .checkLoginId(let request):
             return .requestJSONEncodable(request)
-
-        case .sendResetPasswordEmail(let request):
-            return .requestJSONEncodable(request)
-            
-        case .requestEmailVerification(let request):
-            return .requestJSONEncodable(request)
-
-        case .verifyEmail(let request):
-            return .requestJSONEncodable(request)
-
-        case .resetPassword(let request):
-            return .requestJSONEncodable(request)
-
-        case .reissue,
-             .logout,
-             .withdrawal:
+        case .reissue, .logout, .withdrawal:
             return .requestPlain
         }
     }
-
+    
     var headers: [String: String]? {
         switch self {
         case .reissue:
@@ -115,16 +73,13 @@ extension AuthAPI: TargetType {
                 "Authorization": "Bearer \(Config.accessTK)",
                 "X-Refresh-Token": "\(Config.refreshTK)"
             ]
-
-        case .logout,
-             .withdrawal,
-             .checkLoginId,
-             .resetPassword:
+            
+        case .logout, .withdrawal, .checkLoginId:
             return [
                 "Content-Type": "application/json",
                 "Authorization": "Bearer \(Config.accessTK)"
             ]
-
+            
         default:
             return [
                 "Content-Type": "application/json"
