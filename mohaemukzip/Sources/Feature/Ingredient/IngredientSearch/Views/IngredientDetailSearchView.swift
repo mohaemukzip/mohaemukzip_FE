5caf22d01531c3861a7efbb271521c3e8817fd52
21g <leehan0311@gmail.com>
2026-08-10 14:01:25 +0900

    [Design] 검색 화면의 최근검색어 잘림 수정

--- mohaemukzip/Sources/Feature/Ingredient/IngredientSearch/Views/IngredientDetailSearchView.swift
+++ mohaemukzip/Sources/Feature/Ingredient/IngredientSearch/Views/IngredientDetailSearchView.swift
@@ -61,6 +61,8 @@ struct IngredientDetailSearchView: View {
                                                                  Task { await viewModel.deleteRecent(name: ingredient.keyword) } })
                                     }
                                 }
+                                .padding(.horizontal, 1)
+                                .padding(.vertical, 1)
                             }.padding(.vertical, 16)
                         }
                         
